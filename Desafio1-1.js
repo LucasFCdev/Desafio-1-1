cfae2b97f640721d0bd11d831c438c844595c5bb
LucasFCdev <59072990+LucasFCdev@users.noreply.github.com>
2020-06-19 11:30:42 -0300

    desfio pronto

--- Desafio1-1.js
+++ Desafio1-1.js
@@ -4,3 +4,20 @@
 
 //Constantes recebendo o Dados para indentificação e calculo de IMC
 
+const nome = 'João'
+const peso = 72
+const altura = 1.74
+const sexo = 'M'
+
+// Calculo de IMC
+
+const imc = peso/(altura * altura)
+
+if (imc >= 30){
+    console.log(`${nome}, Seu IMC é: ${imc}, você está acima do peso!`)
+}
+else{
+    console.log(`${nome}, seu IMC é: ${imc}, você não está acima do peso!`)
+}
+
+
